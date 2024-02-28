14c1e2ae16dc25cd8ee81a37d8a3fb4707786ebd
LUIS GUSTAVO OLIVEIRA DA SILVA <luis.gos@puccampinas.edu.br>
2024-02-28 10:32:05 -0300

    Iniciando as Aulas de Python PUC

---
+++ antecessorSucessor.py
@@ -0,0 +1,9 @@
+# Ler Número Inteiro e imprimir Antecessor e o Sucessor
+
+numeroInteiro = int(input('\nDigite o valor inteiro: '))
+
+lerAntecessor = numeroInteiro - 1
+
+lerSucessor = numeroInteiro + 1
+
+print(f"\n\tNúmero inteiro: {numeroInteiro} \n\tAntecessor: {lerAntecessor} \n\tSucessor: {lerSucessor}")
---
+++ calculaPerimetro.py
@@ -0,0 +1,7 @@
+# Calcular Perimetro de um quadrado
+
+lado = float(input('\n\tDigite o valor do lado do quadrado: '))
+
+calcularPerimetro = lado*4
+
+print(f"\n\t\tPerimetro do quadrado: {calcularPerimetro:.3f}")
