bec0f60435679e2de87e502519acdfee237a1fce
LuisSilvah <luisgsilva21@gmail.com>
2024-02-28 10:45:25 -0300

    Calculando a área do triângulo

---
+++ determinarAreaTriangulo.py
@@ -0,0 +1,8 @@
+# Determinar a área do triangulo
+
+baseTriangulo = float(input("\n\tDigite a base do Triângulo: "))
+alturaTriangulo = float(input("\n\tDigite a altura do Triângulo: "))
+
+areaTriangulo = (baseTriangulo * alturaTriangulo) / 2
+
+print(f"\n\t\tResultado da área do Triângulo: {areaTriangulo:.3f}")
